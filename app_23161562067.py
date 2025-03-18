80c004e8aa2263beb3f4b1e4ea5198ac11aa99c2
naufalarya16 <149296792+naufalarya16@users.noreply.github.com>
2025-03-18 13:31:27 +0700

    Add files via upload

---
+++ app_23161562067.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def bfs_shortest_path(city_map, start, goal):
+    if start == goal:
+        return [start]
+    
+    queue = deque([[start]])
+    visited = set()
+
+    while queue:
+        path = queue.popleft() 
+        node = path[-1]  
+
+        if node not in visited:
+            neighbors = city_map.get(node, [])  
+            
+            for neighbor in neighbors:
+                new_path = path + [neighbor] 
+                queue.append(new_path) 
+                
+                if neighbor == goal:
+                    return new_path 
+        
+            visited.add(node) 
+
+    return None 
+
+city_map = {
+    'Home': ['Mall', 'School'],
+    'Mall': ['Gym', 'Hospital'],
+    'School': ['Library'],
+    'Gym': ['Hospital'],
+    'Library': ['Hospital'],
+    'Hospital': []
+}
+
+
+start = 'Home'
+goal = 'Hospital'
+shortest_path = bfs_shortest_path(city_map, start, goal)
+
+print(f"Jalur terpendek dari {start} ke {goal}: {shortest_path}")
